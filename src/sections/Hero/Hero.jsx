2816512260a7f1f712ff148c3c59a33591cf05e9
Edar Velasquez <velasquezedarsw@gmail.com>
2024-07-05 18:07:10 -0400

    added the light/dark  theme changes to the page including icons

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,26 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedinIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg'
+import twitterDark from '../../assets/twitter-dark.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedinLight from '../../assets/linkedin-light.svg'
+import linkedinDark from '../../assets/linkedin-dark.svg'
 import CV from '../../assets/cv.pdf'
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
+    const { theme,toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+
+
   return  (
   <section id="hero" className={styles.container}>
     <div className={styles.colorModeContainer}>
@@ -15,7 +29,7 @@ function Hero() {
         src={heroImg} 
         alt="Profile picture" 
         />
-        <img className={styles.colorMode} src={themeIcon} alt="Color mode Icon" />
+        <img className={styles.colorMode} src={themeIcon} alt="Color mode Icon" onClick={toggleTheme} />
     </div>
     <div className={styles.info}>
         <h1>
