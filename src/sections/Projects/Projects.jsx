458ff119345c43875ae841f772b8e4a561a08735
Edar Velasquez <velasquezedarsw@gmail.com>
2024-08-14 18:05:29 -0400

    finishing Project styling and added Project Card jsx to add projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,29 @@
 import styles from './ProjectsStyles.module.css'
 import viberr from '../../assets/viberr.png'
+import freshBurger from '../../assets/fresh-burger.png'
+import hipsster from '../../assets/hipsster.png'
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
   <section id="projects" className={styles.container}>
   <h1 className='sectionTitle'>Projects</h1>
   <div className={styles.projectsContainer}>
-    <a href="" target='_blank'>
-        <img className='hover' src={viberr} alt=""></img>
-        <h3>Viberr</h3>
-        <p>Streaming App</p>
-    </a>
+    <ProjectCard src={viberr} 
+    link=""
+    h3="Viberr"
+    p="Streaming App"
+    />
+    <ProjectCard src={freshBurger} 
+    link=""
+    h3="Fresh Burger"
+    p="Hamburger Restaraunt"
+    />
+    <ProjectCard src={hipsster} 
+    link=""
+    h3="Hipsster"
+    p="Glasses Shop"
+    />
   </div>
   </section>
   );
