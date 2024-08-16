ff3499b60aff7ae5a0ef7cded63734745c11ecb3
Edar Velasquez <velasquezedarsw@gmail.com>
2024-08-16 10:58:59 -0400

    adding skills component

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,36 @@
+import SkillList from '../../common/SkillList';
+import styles from './SkillsStyles.module.css';
+import checkMarkIcon from '../../assets/checkmark-dark.svg';
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+        <h1>Skills</h1>
+        <div className={styles.skillList}>
+                <SkillList src={checkMarkIcon} skill="HTML"/>
+                <SkillList src={checkMarkIcon} skill="CSS"/>
+                <SkillList src={checkMarkIcon} skill="JavaScript"/>
+                <SkillList src={checkMarkIcon} skill="TypeScript"/>
+                <SkillList src={checkMarkIcon} skill="Node"/>
+        </div>
+        <hr />
+        <div className={styles.skillList}>
+                <SkillList src={checkMarkIcon} skill="React"/>
+                <SkillList src={checkMarkIcon} skill="Angular"/>
+                <SkillList src={checkMarkIcon} skill="Vue"/>
+                <SkillList src={checkMarkIcon} skill="Tailwind CSS"/>
+        </div>
+        <hr />
+        <hr />
+        <div className={styles.skillList}>
+                <SkillList src={checkMarkIcon} skill="Redux"/>
+                <SkillList src={checkMarkIcon} skill="Webpack"/>
+                <SkillList src={checkMarkIcon} skill="Git"/>
+                <SkillList src={checkMarkIcon} skill="Jest"/>
+        </div>
+        <hr />
+    </section>
+  );
+}
+
+export default Skills;
